075f9fff791c3b51d19c3a0e03c2f85de13e6b10
HyojinKim <gywls00100@gmail.com>
2021-11-23 03:55:11 +0900

    next.config 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  env: {
+    API_ENDPOINT: process.env.IP_ADDRESS,
+  },
   reactStrictMode: true,
-}
+  images: {
+    domains: ['c2.poing.co.kr'],
+  },
+};
