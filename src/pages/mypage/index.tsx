1d51d9112a2cc532aaaeaf788f2f8ff7fc8427bd
HyojinKim <gywls00100@gmail.com>
2021-11-26 04:21:36 +0900

    마이페이지

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import React from 'react';
 
-import Layout from '@components/Layout';
+import { Layout } from '@components/Layout';
 
 const MyPage: NextPage = () => {
-  return <Layout title="마이페이지">dd</Layout>;
+  return (
+    <Layout title="마이페이지">
+      <Image src="/images/mypage-certificate.jpg" width="375" height="812" />
+    </Layout>
+  );
 };
 
 export default MyPage;
