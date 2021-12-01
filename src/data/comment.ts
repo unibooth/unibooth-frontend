ce06a4933ef1a33eb4fdabd6f624be8888364be8
HyojinKim <gywls00100@gmail.com>
2021-12-02 00:42:12 +0900

    Add dummy data

--- src/data/comment.ts
+++ src/data/comment.ts
@@ -1,4 +1,39 @@
 import { Comment } from '@interfaces';
 
 // @TODO: 더미데이터 채우기!
-export const COMMENT_DATA: Comment[] = [];
+export const COMMENT_DATA: Comment[] = [
+  {
+    id: 1,
+    createdAt: new Date(),
+    /** 댓글 내용 */
+    content: '한나님 일러스트 색감 너무 예뻐요!',
+
+    user: {
+      id: 1,
+      nickname: '효진',
+      imageUrl: '', //댓글 단 사람 이미지인가요?
+      univercity: '중앙대학교',
+      /** 학교 인증이 완료되었는가 */
+      isVerified: true,
+    },
+
+    boothId: 1,
+  },
+  {
+    id: 2,
+    createdAt: new Date(),
+    /** 댓글 내용 */
+    content: '한나님 일러스트 색감 너무 예뻐요!',
+
+    user: {
+      id: 2,
+      nickname: '한나님팬',
+      imageUrl: '', //댓글 단 사람 이미지인가요?
+      univercity: '중앙대학교',
+      /** 학교 인증이 완료되었는가 */
+      isVerified: true,
+    },
+
+    boothId: 1,
+  },
+];
