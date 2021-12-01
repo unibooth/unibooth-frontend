618fb91a132e3cdad807b8d5276d12c43e64aac8
greatSumini <greatSumini@gmail.com>
2021-12-02 00:05:13 +0900

    perf(styles): remove @font-face css

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,13 +4,6 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
-  @font-face {
-    font-family: 'Apple SD Gothic Neo';
-    src: url('//db.onlinewebfonts.com/t/67680f2eb947c5cbd58b40961b4a61e9.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
-  }
-
   * {
     font-family: 'Apple SD Gothic Neo', sans-serif !important;
     font-style: normal;
