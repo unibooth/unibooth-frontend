01abfeb2a0c0b7c340b027925d0b365a4416e1c8
greatSumini <greatSumini@gmail.com>
2021-12-02 00:50:44 +0900

    chore(): fix comment data conflict

--- src/data/comment.ts
+++ src/data/comment.ts
@@ -28,33 +28,3 @@ export const COMMENT_DATA: Comment[] = [
     boothId: 2,
   },
 ];
-
-    user: {
-      id: 1,
-      nickname: '효진',
-      imageUrl: '', //댓글 단 사람 이미지인가요?
-      univercity: '중앙대학교',
-      /** 학교 인증이 완료되었는가 */
-      isVerified: true,
-    },
-
-    boothId: 1,
-  },
-  {
-    id: 2,
-    createdAt: new Date(),
-    /** 댓글 내용 */
-    content: '한나님 일러스트 색감 너무 예뻐요!',
-
-    user: {
-      id: 2,
-      nickname: '한나님팬',
-      imageUrl: '', //댓글 단 사람 이미지인가요?
-      univercity: '중앙대학교',
-      /** 학교 인증이 완료되었는가 */
-      isVerified: true,
-    },
-
-    boothId: 1,
-  },
-];
