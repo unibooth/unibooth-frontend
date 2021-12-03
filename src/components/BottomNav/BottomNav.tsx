e6947de66c00aab01a2cee1bca86bcdf1723fa6e
HyojinKim <gywls00100@gmail.com>
2021-12-03 15:10:32 +0900

    feat: change icon

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-import Alram from '@assets/alarm.svg';
-import Chat from '@assets/chat.svg';
+import Feed from '@assets/feed.svg';
 import Home from '@assets/home.svg';
-import Loc from '@assets/loc.svg';
+import Map from '@assets/map.svg';
+import Message from '@assets/message.svg';
 import MyPage from '@assets/mypage.svg';
 
 import * as S from './styles';
@@ -21,22 +21,22 @@ const BottomNav = () => {
       </Link>
 
       <Link href="/notification" passHref>
-        <a className={`nav-link ${router.asPath === '/notification' ? 'active' : ''}`}>
-          <Alram className="svg-paths" style={{ fontSize: '18px' }} />
+        <a className={`nav-link ${router.asPath === '/feed' ? 'active' : ''}`}>
+          <Feed className="svg-paths" style={{ fontSize: '18px' }} />
           <span>축제피드</span>
         </a>
       </Link>
 
       <Link href="/map" passHref>
         <a className={`nav-link ${router.asPath === '/map' ? 'active' : ''}`}>
-          <Loc className="svg-paths" style={{ fontSize: '18px' }} />
+          <Map className="svg-paths" style={{ fontSize: '18px' }} />
           <span>지도</span>
         </a>
       </Link>
 
       <Link href="/chat" passHref>
-        <a className={`nav-link ${router.asPath === '/chat' ? 'active' : ''}`}>
-          <Chat className="svg-paths" style={{ fontSize: '18px' }} />
+        <a className={`nav-link ${router.asPath === '/message' ? 'active' : ''}`}>
+          <Message className="svg-paths" style={{ fontSize: '18px' }} />
           <span>메시지</span>
         </a>
       </Link>
