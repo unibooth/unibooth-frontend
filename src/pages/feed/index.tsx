c1dd761f2d34fb09be98395c606c708625f8e24e
HyojinKim <gywls00100@gmail.com>
2021-12-04 16:29:02 +0900

    feat: 축제피드 헤더 작성

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,10 +1,32 @@
 import type { NextPage } from 'next';
 import React from 'react';
+import styled from 'styled-components';
 
+import Search from '@assets/feed-search.svg';
 import { Layout } from '@components/Layout';
 
 const Feed: NextPage = () => {
-  return <Layout title="알림">알림</Layout>;
+  return (
+    <Layout title="알림" noHeader>
+      <Header>
+        <div>축제피드</div>
+        <Search />
+      </Header>
+    </Layout>
+  );
 };
 
 export default Feed;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  height: 44px;
+  font-family: Apple SD Gothic Neo;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+`;
