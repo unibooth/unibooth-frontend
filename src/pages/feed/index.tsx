848856353c7251fe51ae337e911e2190f2758170
HyojinKim <gywls00100@gmail.com>
2021-12-04 19:05:23 +0900

    feat: 축제피드 플로팅버튼 추가

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Comment from '@assets/feed-comment.svg';
+import Create from '@assets/feed-create.svg';
 import Search from '@assets/feed-search.svg';
 import { Layout } from '@components/Layout';
 import { FEED_CONTENT_DATA } from '@data';
@@ -35,6 +36,9 @@ const Feed: NextPage = () => {
           </div>
         </FeedWrapper>
       ))}
+      <FloatCreateButton>
+        <Create />
+      </FloatCreateButton>
     </Layout>
   );
 };
@@ -96,4 +100,18 @@ const FeedWrapper = styled.div`
     line-height: 12px;
     color: #818798;
   }
+`;
+
+const FloatCreateButton = styled.div`
+  background: #ff6433;
+  border-radius: 12px;
+  position: fixed;
+  left: 315px;
+  top: 690px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 44px;
+  height: 44px;
+  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.2));
 `;
