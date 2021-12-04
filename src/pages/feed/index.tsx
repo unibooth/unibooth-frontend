ef6184c17441448c54a62373b1bde48f81c91034
HyojinKim <gywls00100@gmail.com>
2021-12-04 16:34:57 +0900

    feat: 축제피드 카테고리 작성

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -12,6 +12,12 @@ const Feed: NextPage = () => {
         <div>축제피드</div>
         <Search />
       </Header>
+      <CategoryWrapper>
+        <div className="tab">체험부스</div>
+        <div className="tab">마켓부스</div>
+        <div className="tab">공연</div>
+        <div className="tab">기타</div>
+      </CategoryWrapper>
     </Layout>
   );
 };
@@ -29,4 +35,27 @@ const Header = styled.div`
   font-weight: 600;
   font-size: 20px;
   line-height: 24px;
+`;
+const CategoryWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  padding: 0 16px;
+  height: 52px;
+  background: #f1f2f3;
+
+  .tab {
+    font-family: Apple SD Gothic Neo;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 18px;
+    text-align: center;
+    padding: 9px 16px;
+    background: #ffffff;
+    border: 0.5px solid #d5d7dd;
+    box-sizing: border-box;
+    border-radius: 22px;
+  }
 `;
