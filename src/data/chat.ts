a0f87e907434515c3857615f6810779fe86ff05a
HyojinKim <gywls00100@gmail.com>
2021-12-04 23:04:27 +0900

    feat: chat 데이터 넣기

---
+++ src/data/chat.ts
@@ -0,0 +1,32 @@
+import { Chat } from '@interfaces';
+
+export const CHAT_DATA: Chat[] = [
+  {
+    id: 1,
+    profileUrl: '/images/chat-profile1.png',
+    name: 'Hannah',
+    content: '안녕하세요😊 전부 방수 코팅 되어 있습니다!',
+    createdAt: '10월 20일',
+  },
+  {
+    id: 2,
+    profileUrl: '/images/chat-profile2.png',
+    name: '식금술사',
+    content: '솔티 카라멜 재고 있습니다! 5개요!!',
+    createdAt: '5월 28일',
+  },
+  {
+    id: 3,
+    profileUrl: '/images/chat-profile3.png',
+    name: '주미',
+    content: '925 실버 제품은 솔드아웃이에요 ㅠ.ㅠ 죄송 ...',
+    createdAt: '5월 28일',
+  },
+  {
+    id: 4,
+    profileUrl: '/images/chat-profile4.png',
+    name: '자연인',
+    content: '부스 오픈 시간은 5시부터예요! 수업이 있어 ...',
+    createdAt: '5월 27일',
+  },
+];
